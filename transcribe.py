c2ad3070b5bc247e2578959f2e534f66b4507496
FrostyAqua37 <eivindc@uia.no>
2026-09-17 00:26:38 +0200

    Added new video, font file, and some code changes.

--- transcribe.py
+++ transcribe.py
@@ -1,8 +1,23 @@
 import whisper
 import sys
+from moviepy import *
+import numpy as np
 
-media = sys.argv[1]
-model = whisper.load_model("base")
-transcribed_media = model.transcribe(media, fp16=False)
+def get_subtitles(media:str, timestamp:bool=False) -> str | dict[str, str | list]:
+    model = whisper.load_model("medium")
+    transcribed_media = model.transcribe(media, fp16=False)
 
-print(transcribed_media['segments'])
+    if timestamp:
+        return transcribed_media['segments']
+
+    return transcribed_media['text']
+
+"""
+def dynamic_var(var_name:str='subtitle1', value:str='') -> str:
+    #Make dynamic variable with passed string
+    exec(var_name + f' = "{value}"')
+    return subtitle1
+"""
+
+def format_timestamp(subtitles, start:float, end:float, font:str="resources/Florence.ttf", font_size:int=28, color:str='#fff'):
+    ...
