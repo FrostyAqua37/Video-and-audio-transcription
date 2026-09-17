47bdbeddc86af7b239c65c468947c7f6d7930962
FrostyAqua37 <eivindc@uia.no>
2026-09-17 19:05:28 +0200

    Added font.ttf file and timestamped transcripted text.

--- transcribe.py
+++ transcribe.py
@@ -4,20 +4,43 @@ from moviepy import *
 import numpy as np
 
 def get_subtitles(media:str, timestamp:bool=False) -> str | dict[str, str | list]:
-    model = whisper.load_model("medium")
+    model = whisper.load_model("large-v3")
     transcribed_media = model.transcribe(media, fp16=False)
 
     if timestamp:
+        #Returns transcribed text with timestamp if 'timestamp=True'
         return transcribed_media['segments']
 
+    #Returns only transcribed text
     return transcribed_media['text']
 
+def format_timestamp(subtitle:str, font:str="resources/BebasNeue-Regular.ttf", font_size:int=28, color:str='#fff'):
+    #Formats transcribed text to fit video.
+    return TextClip(
+            font=font,
+            text=subtitle,
+            font_size=font_size,
+            color=color,
+            text_align='center'
+            )
+
+#subtitles = get_subtitles(sys.argv[1], timestamp=True)  #Gets video file location from terminal and passes it to function
+#subtitle_list = []
+
 """
-def dynamic_var(var_name:str='subtitle1', value:str='') -> str:
-    #Make dynamic variable with passed string
-    exec(var_name + f' = "{value}"')
-    return subtitle1
+for subtitle in subtitles:
+    #Loops through all the transcribed text and sets start and end duration. 
+    subtitle_list.append(format_timestamp(subtitle=subtitle['text']))   
+    subtitle_list[subtitle['id']].with_start(subtitle['start']).with_end(subtitle['end'])   
+    subtitle_list[subtitle['id']].with_position(('center', 'bottom'))
+
+media = VideoFileClip('resources/youtube-audio.mp4')
+media.with_start(1).with_end(184.26)
+subtitle_list.insert(0, media)
+video_transcribed = CompositeVideoClip(subtitle_list)
+video_transcribed.preview(fps=20)
 """
 
-def format_timestamp(subtitles, start:float, end:float, font:str="resources/Florence.ttf", font_size:int=28, color:str='#fff'):
-    ...
+media = VideoFileClip(sys.argv[1])
+media = media.subclipped(1, 60)
+media.preview()
